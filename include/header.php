b83edab34728bed364fb7f7b8bdbe4b1bba774c9
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-04-18 00:02:47 +0100

    menu

--- include/header.php
+++ include/header.php
@@ -14,10 +14,12 @@
 
 <body>
 
-<header class="rect_logo">
+<section class="rect_logo">
     <div class="logo_container">
-        <a href="index.php"><img src="Images/logo.svg"></a>            
+        <a href="index.php"><img src="Images/logo.svg">
         <p>Match Me</p>
+        </a>            
+        
     </div>
     <div class="search">
         <form class="rect_search" action="/action_page.php">
@@ -25,4 +27,49 @@
             <button type="submit"><i class="fa fa-search"></i></button>
         </form>
     </div>
-</header>
+</section>
+
+<section class="menu_body_sug">
+<div class="menu_gauche">
+    <div class="menu_elements Accueil">
+        <a href="index.php"><img src="Images/house.svg" alt="">
+        <p>Accueil</p>
+        </a>
+    </div>
+    <div class="menu_elements Messages">
+        <a href="index.php"><img src="Images/house.svg" alt="">
+        <p>Messages</p>
+        </a>
+    </div>
+    <div class="menu_elements Messages">
+        <a href="index.php"><img src="Images/house.svg" alt="">
+        <p>Notifications</p>
+        </a>
+    </div>
+    <div class="menu_elements Messages">
+        <a href="index.php"><img src="Images/house.svg" alt="">
+        <p>Découvrir</p>
+        </a>
+    </div>
+    <div class="menu_elements Messages">
+        <a href="index.php"><img src="Images/house.svg" alt="">
+        <p>Poster</p>
+        </a>
+    </div>
+    <div class="menu_elements Messages">
+        <a href="index.php"><img src="Images/house.svg" alt="">
+        <p>Favoris</p>
+        </a>
+    </div>
+    <div class="menu_elements Messages">
+        <a href="index.php"><img src="Images/house.svg" alt="">
+        <p>Profil</p>
+        </a>
+    </div>
+    <div class="menu_elements Messages">
+        <a href="index.php"><img src="Images/house.svg" alt="">
+        <p>Paramètres</p>
+        </a>
+    </div>
+
+</div>
