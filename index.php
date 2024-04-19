7676212a597e8dbe98ec8bcf35e216d3a0999d65
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-04-19 09:52:47 +0100

    menu droit

--- index.php
+++ index.php
@@ -7,8 +7,55 @@ include('include/header.php')
 </div>
 
 <div class="menu_droite">
-    <div class="Suggestions">
-        <h1>Suggestions</h1>
+    <div class="menu_droit_elements">
+        <div class="menu_droit_suggestion">
+            <div class="Suggestions">
+                <h1>Suggestions</h1>
+            </div>
+            <div class="profil_suggestion">
+                <a href="index.php"><img src="Images/lune.png">
+                <span class="online"></span>
+                <p>KHALIL OBA Adef</p>
+                </a>
+            </div>
+            <div class="profil_suggestion">
+                <a href="index.php"><img src="Images/lune.png">
+                <span class="online"></span>
+                <p>KHALIL OBA Adef</p>
+                </a>
+            </div>
+            <div class="profil_suggestion">
+                <a href="index.php"><img src="Images/lune.png">
+                <span class="online"></span>
+                <p>KHALIL OBA Adef</p>
+                </a>
+            </div>
+        </div>
+
+        <div class="menu_droit_contact">
+            <div class="contacts">
+                <h1>Contacts</h1>
+            </div>
+            <div class="profil_suggestion">
+                <a href="index.php"><img src="Images/lune.png">
+                <span class="online"></span>
+                <p>KHALIL OBA Adef</p>
+                </a>
+            </div>
+            <div class="profil_suggestion">
+                <a href="index.php"><img src="Images/lune.png">
+                <span class="online"></span>
+                <p>Francis-ny Itoua</p>
+                </a>
+            </div>
+            <div class="profil_suggestion">
+                <a href="index.php"><img src="Images/lune.png">
+                <span class="online"></span>
+                <p>Sam-Kessy Christopher Nicolas KOUNGA IBINDA</p>
+                </a>
+            </div>
+        </div>
+        
     </div>
 </div>
 
