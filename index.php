87bac240b48cbae0beeb970bc40a16e83da87f31
Francisny03 <120179564+Francisny03@users.noreply.github.com>
2024-04-25 08:55:21 +0100

    page d'accueil

--- index.php
+++ index.php
@@ -38,20 +38,228 @@ include('include/header.php')
                     </p>
                 </div>
                 <br>
-                <br>
-                <div class="bouton_actions btn_like_dislike_etc">
-                    <div class=""></div>
-
+                <div class="bouton_actions btn_all">
+                    <div class="btn_dislike btn_all">
+                        <img src="Images/x.svg" alt="">
+                    </div>
+                    <div class="btn_match btn_all">
+                        <img src="Images/link.svg" alt="">
+                    </div>
+                    <div class="btn_heart btn_all">
+                        <img src="Images/heart.svg" alt="">
+                    </div>
+                    <div class="btn_favoris btn_all">
+                        <img src="Images/favoris.svg" alt="">
+                    </div>
                 </div>
-
-
             </div>
-
         </div>
-
+        <br>
+        <div class="rect_milieu">
+            <div class="rect_milieu_photo">
+            </div>
+            <div class="rect_milieu_info">
+                <div class="name_age_city_online">
+                    <p class="name"> Francis-ny Itoua</p>
+                    <p class="age"> 25 ans</p>
+                    <p class="city">
+                        <img src="Images/placeholder.svg" alt=""> Brazzaville
+                    </p>
+                    <p class="online"> En Ligne</p>
+                    <div class="border_separate"></div>
+                </div>
+                <div class="search_for interet_recherche">
+                    <h1>Recherche</h1>
+                    <p>Relation court-terme</p>
+                    <p>Amitié</p>
+                    <p>Business</p>
+                    <div class="border_separate"></div>
+                </div>
+                <div class="interet interet_recherche">
+                    <h1>Centre d'intérêts</h1>
+                    <p>Sport</p>
+                    <p>Mangas</p>
+                    <p>Films</p>
+                    <p>Séries</p>
+                    <p>Romans</p>
+                    <p>Musique</p>
+                </div>
+                <div class="description">
+                    <p>Beau gosse, intelligent, vanneur, drôle, un peu borderline...
+                    </p>
+                </div>
+                <br>
+                <div class="bouton_actions btn_all">
+                    <div class="btn_dislike btn_all">
+                        <img src="Images/x.svg" alt="">
+                    </div>
+                    <div class="btn_match btn_all">
+                        <img src="Images/link.svg" alt="">
+                    </div>
+                    <div class="btn_heart btn_all">
+                        <img src="Images/heart.svg" alt="">
+                    </div>
+                    <div class="btn_favoris btn_all">
+                        <img src="Images/favoris.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="rect_milieu">
+            <div class="rect_milieu_photo">
+            </div>
+            <div class="rect_milieu_info">
+                <div class="name_age_city_online">
+                    <p class="name"> Francis-ny Itoua</p>
+                    <p class="age"> 25 ans</p>
+                    <p class="city">
+                        <img src="Images/placeholder.svg" alt=""> Brazzaville
+                    </p>
+                    <p class="online"> En Ligne</p>
+                    <div class="border_separate"></div>
+                </div>
+                <div class="search_for interet_recherche">
+                    <h1>Recherche</h1>
+                    <p>Relation court-terme</p>
+                    <p>Amitié</p>
+                    <p>Business</p>
+                    <div class="border_separate"></div>
+                </div>
+                <div class="interet interet_recherche">
+                    <h1>Centre d'intérêts</h1>
+                    <p>Sport</p>
+                    <p>Mangas</p>
+                    <p>Films</p>
+                    <p>Séries</p>
+                    <p>Romans</p>
+                    <p>Musique</p>
+                </div>
+                <div class="description">
+                    <p>Beau gosse, intelligent, vanneur, drôle, un peu borderline...
+                    </p>
+                </div>
+                <br>
+                <div class="bouton_actions btn_all">
+                    <div class="btn_dislike btn_all">
+                        <img src="Images/x.svg" alt="">
+                    </div>
+                    <div class="btn_match btn_all">
+                        <img src="Images/link.svg" alt="">
+                    </div>
+                    <div class="btn_heart btn_all">
+                        <img src="Images/heart.svg" alt="">
+                    </div>
+                    <div class="btn_favoris btn_all">
+                        <img src="Images/favoris.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="rect_milieu">
+            <div class="rect_milieu_photo">
+            </div>
+            <div class="rect_milieu_info">
+                <div class="name_age_city_online">
+                    <p class="name"> Francis-ny Itoua</p>
+                    <p class="age"> 25 ans</p>
+                    <p class="city">
+                        <img src="Images/placeholder.svg" alt=""> Brazzaville
+                    </p>
+                    <p class="online"> En Ligne</p>
+                    <div class="border_separate"></div>
+                </div>
+                <div class="search_for interet_recherche">
+                    <h1>Recherche</h1>
+                    <p>Relation court-terme</p>
+                    <p>Amitié</p>
+                    <p>Business</p>
+                    <div class="border_separate"></div>
+                </div>
+                <div class="interet interet_recherche">
+                    <h1>Centre d'intérêts</h1>
+                    <p>Sport</p>
+                    <p>Mangas</p>
+                    <p>Films</p>
+                    <p>Séries</p>
+                    <p>Romans</p>
+                    <p>Musique</p>
+                </div>
+                <div class="description">
+                    <p>Beau gosse, intelligent, vanneur, drôle, un peu borderline...
+                    </p>
+                </div>
+                <br>
+                <div class="bouton_actions btn_all">
+                    <div class="btn_dislike btn_all">
+                        <img src="Images/x.svg" alt="">
+                    </div>
+                    <div class="btn_match btn_all">
+                        <img src="Images/link.svg" alt="">
+                    </div>
+                    <div class="btn_heart btn_all">
+                        <img src="Images/heart.svg" alt="">
+                    </div>
+                    <div class="btn_favoris btn_all">
+                        <img src="Images/favoris.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="rect_milieu">
+            <div class="rect_milieu_photo">
+            </div>
+            <div class="rect_milieu_info">
+                <div class="name_age_city_online">
+                    <p class="name"> Francis-ny Itoua</p>
+                    <p class="age"> 25 ans</p>
+                    <p class="city">
+                        <img src="Images/placeholder.svg" alt=""> Brazzaville
+                    </p>
+                    <p class="online"> En Ligne</p>
+                    <div class="border_separate"></div>
+                </div>
+                <div class="search_for interet_recherche">
+                    <h1>Recherche</h1>
+                    <p>Relation court-terme</p>
+                    <p>Amitié</p>
+                    <p>Business</p>
+                    <div class="border_separate"></div>
+                </div>
+                <div class="interet interet_recherche">
+                    <h1>Centre d'intérêts</h1>
+                    <p>Sport</p>
+                    <p>Mangas</p>
+                    <p>Films</p>
+                    <p>Séries</p>
+                    <p>Romans</p>
+                    <p>Musique</p>
+                </div>
+                <div class="description">
+                    <p>Beau gosse, intelligent, vanneur, drôle, un peu borderline...
+                    </p>
+                </div>
+                <br>
+                <div class="bouton_actions btn_all">
+                    <div class="btn_dislike btn_all">
+                        <img src="Images/x.svg" alt="">
+                    </div>
+                    <div class="btn_match btn_all">
+                        <img src="Images/link.svg" alt="">
+                    </div>
+                    <div class="btn_heart btn_all">
+                        <img src="Images/heart.svg" alt="">
+                    </div>
+                    <div class="btn_favoris btn_all">
+                        <img src="Images/favoris.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
     </div>
-
-
 </div>
 
 <div class="menu_droite">
